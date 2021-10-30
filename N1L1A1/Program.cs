923d1d28fdf8720e0e2a20aec8de2407847d4af0
J. Estuardo Telguario <estuardotelejt@gmail.com>
2021-10-29 19:16:26 -0600

    Nivel 1 - Leccion 1 - Actividad 1

---
+++ N1L1A1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AooAreaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double var1 = 34.6, var2 = 65.4;
+
+            Console.WriteLine("El resultado de la suma es: " + (var1 + var2));
+            Console.ReadKey();
+        }
+    }
+}
