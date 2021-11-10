8af8d54a340c99d92090e69ce3416588ac8569cf
J. Estuardo Telguario <estuardotelejt@gmail.com>
2021-11-09 23:10:33 -0600

    Nivel 1 - Leccion 2 - Actividad 1

--- N1L1A1/Program.cs
+++ N1L1A1/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 
-namespace AooAreaCirculo
+namespace ActividadesCursoCSharp
 {
     class Program
     {
         static void Main(string[] args)
         {
             double var1 = 34.6, var2 = 65.4;
-
             Console.WriteLine("El resultado de la suma es: " + (var1 + var2));
             Console.ReadKey();
         }
---
+++ N1L2A1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ActividadesCursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacciSerie = new int[100];
+            fibonacciSerie[0]++;
+            fibonacciSerie[1]++;
+            for (int i = 2; i < fibonacciSerie.Length; i++)
+            {
+                fibonacciSerie[i] = (fibonacciSerie[i - 1] + fibonacciSerie[i - 2]);
+            }
+            printFibonacciSerie(ref fibonacciSerie);
+            Console.ReadKey();
+        }
+
+        static void printFibonacciSerie(ref int[] fibonacciSerie)
+        {
+            for (int i = 0; i < fibonacciSerie.Length; i++)
+            {
+                Console.WriteLine(fibonacciSerie[i]);
+            }
+        }
+    }
+}
