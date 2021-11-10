0a4c6ccf7d81c6068ff305b078c8ef4d6a26bd4e
J. Estuardo Telguario <estuardotelejt@gmail.com>
2021-11-09 23:14:06 -0600

    Nivel 1 - Leccion 2 - Actividad 1

--- N1L2A1/Program.cs
+++ N1L2A1/Program.cs
@@ -7,11 +7,16 @@ namespace ActividadesCursoCSharp
         static void Main(string[] args)
         {
             int[] fibonacciSerie = new int[100];
-            fibonacciSerie[0]++;
-            fibonacciSerie[1]++;
-            for (int i = 2; i < fibonacciSerie.Length; i++)
+            for (int i = 0; i < fibonacciSerie.Length; i++)
             {
-                fibonacciSerie[i] = (fibonacciSerie[i - 1] + fibonacciSerie[i - 2]);
+                if (i == 0 || i == 1)
+                {
+                    fibonacciSerie[i]++;
+                }
+                else
+                {
+                    fibonacciSerie[i] = (fibonacciSerie[i - 1] + fibonacciSerie[i - 2]);
+                }
             }
             printFibonacciSerie(ref fibonacciSerie);
             Console.ReadKey();
